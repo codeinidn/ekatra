09d7d00a8c0124923eeda883401dcbfd5d21cad1
codeinidn <codeinidn@gmail.com>
2023-12-12 16:36:33 +0700

    full responsive

--- src/Pages/Home.jsx
+++ src/Pages/Home.jsx
@@ -6,7 +6,7 @@ export default function Home() {
     <div>
       <div className='hero'>
         <div className='logo'>
-            <img src="./ekatra.png" alt="" />
+            <img src="./ekatra.png" alt="ekatra-logo" className='ekatra-logo'/>
         </div>
         <div className="vertical-line"></div>
         <div className='slogan'>
@@ -22,18 +22,18 @@ export default function Home() {
         <div className='engagement-price-list'>
           <div className='price-list-1'>
             <p className='p'>PHOTO VIDEO | <span className='black'>IDR 2,5</span></p>
-            <p>1 PHOTOGRAPHER & 1 VIDEOGRAPHER</p>
-            <p>2 WORKHOURS</p>
-            <p>50+ edited photos</p>
-            <p>1 MIN VIDEO TEASER</p>
-            <p>ALL FILES VIA GOOGLE DRIVE</p>
+            <p className='list'>1 PHOTOGRAPHER & 1 VIDEOGRAPHER</p>
+            <p className='list'>2 WORKHOURS</p>
+            <p className='list'>50+ edited photos</p>
+            <p className='list'>1 MIN VIDEO TEASER</p>
+            <p className='list'>ALL FILES VIA GOOGLE DRIVE</p>
           </div>
           <div className='price-list-2'>
             <p className='p'>PHOTO ONLY | <span className='black'>IDR 1,7</span></p>
-            <p>1 PHOTOGRAPHER + ASSIST</p>
-            <p>2 WORKHOURS</p>
-            <p>25+ edited photos</p>
-            <p>ALL FILES VIA GOOGLE DRIVE</p>
+            <p className='list'>1 PHOTOGRAPHER + ASSIST</p>
+            <p className='list'>2 WORKHOURS</p>
+            <p className='list'>25+ edited photos</p>
+            <p className='list'>ALL FILES VIA GOOGLE DRIVE</p>
           </div>
         </div>
       </div>
@@ -46,21 +46,21 @@ export default function Home() {
         <div className='engagement-price-list'>
           <div className='price-list-1'>
             <p className='p'>PHOTO VIDEO | <span className='gold'>IDR 2,8</span></p>
-            <p>1 PHOTOGRAPHER & 1 VIDEOGRAPHER</p>
-            <p>1 MINIMALIST 16RP FRAME</p>
-            <p>1 TO 2 MINUTES VIDEO DURATION</p>
-            <p>MAX 2 OUTFITS</p>
-            <p>50- EDITED PHOTO</p>
-            <p>ALL FILES VIA GOOGLE DRIVE</p>
+            <p className='list'>1 PHOTOGRAPHER & 1 VIDEOGRAPHER</p>
+            <p className='list'>1 MINIMALIST 16RP FRAME</p>
+            <p className='list'>1 TO 2 MINUTES VIDEO DURATION</p>
+            <p className='list'>MAX 2 OUTFITS</p>
+            <p className='list'>50- EDITED PHOTO</p>
+            <p className='list'>ALL FILES VIA GOOGLE DRIVE</p>
           </div>
           <div className='price-list-2'>
             <p className='p'>PHOTO ONLY | <span className='gold'>IDR 2</span></p>
-            <p>1 PHOTOGRAPHER = 1 Assist</p>
-            <p>1 MINIMALIST 16RP FRAME</p>
-            <p>4 workhours</p>
-            <p>MAX 2 OUTFITS</p>
-            <p>50 EDITED PHOTO</p>
-            <p>ALL FILES VIA GOOGLE DRIVE</p>
+            <p className='list'>1 PHOTOGRAPHER = 1 Assist</p>
+            <p className='list'>1 MINIMALIST 16RP FRAME</p>
+            <p className='list'>4 workhours</p>
+            <p className='list'>MAX 2 OUTFITS</p>
+            <p className='list'>50 EDITED PHOTO</p>
+            <p className='list'>ALL FILES VIA GOOGLE DRIVE</p>
           </div>
         </div>
       </div>
